16a51f8bc9bce0e941ae76ad9e43d514cf18e8db
Jayani2003 <jayanimadusha23@gmail.com>
2025-04-17 23:21:04 +0530

    Add Timetable GUIs for Admin,User,Technical officer and Add User Image Field to user table

--- src/main/java/com/lms/bytecoders/Controllers/Admin/AdminTimeTableController.java
+++ src/main/java/com/lms/bytecoders/Controllers/Admin/AdminTimeTableController.java
@@ -1,4 +1,42 @@
 package com.lms.bytecoders.Controllers.Admin;
 
-public class AdminTimeTableController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminTimeTableController implements Initializable {
+
+    @FXML
+    private ComboBox<String> combolevel;
+
+    @FXML
+    private ComboBox<String> combosemester;
+
+    @FXML
+    private ComboBox<String> combodepartment;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Set combo box items
+        ObservableList<String> listlevel = FXCollections.observableArrayList("Level 1", "Level 2","Level 3","Level 4");
+        combolevel.setItems(listlevel);
+
+        ObservableList<String> listsemester = FXCollections.observableArrayList("Semester 1", "Semester 2");
+        combosemester.setItems(listsemester);
+
+        ObservableList<String> listdepartment = FXCollections.observableArrayList("ICT", "ET","BST");
+        combodepartment.setItems(listdepartment);
+
+
+
+
+    }
 }
