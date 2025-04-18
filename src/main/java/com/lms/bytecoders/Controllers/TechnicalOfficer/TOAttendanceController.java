155e66ffe9165c82572482c44093ceaeb53dca9c
Gayanath <kolithagrajapaksha@gmail.com>
2025-04-18 20:36:03 +0530

    Add GUI  interfaces for Technical Officer Attendance, Medical, and Admin Course module.

--- src/main/java/com/lms/bytecoders/Controllers/TechnicalOfficer/TOAttendanceController.java
+++ src/main/java/com/lms/bytecoders/Controllers/TechnicalOfficer/TOAttendanceController.java
@@ -1,4 +1,30 @@
 package com.lms.bytecoders.Controllers.TechnicalOfficer;
 
-public class TOAttendanceController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TOAttendanceController implements Initializable {
+
+    @FXML
+    private ComboBox<String> comboBox1;
+
+    @FXML
+    private ComboBox<String> comboBox2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Set combo box items
+        ObservableList<String> listlevel = FXCollections.observableArrayList("THEORY", "PRACTICAL");
+        comboBox1.setItems(listlevel);
+
+        ObservableList<String> listlevel2 = FXCollections.observableArrayList("YES", "NO");
+        comboBox2.setItems(listlevel2);
+
+    }
 }
