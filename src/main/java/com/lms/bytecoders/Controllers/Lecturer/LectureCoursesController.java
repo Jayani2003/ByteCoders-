0027fdc936f74ff3ff8a48427370881d186c564f
Amila619 <amilasandeepa349@gmail.com>
2025-04-24 23:36:00 +0530

    - created student and lecture course views , fixed a few issues in lecture and course time table

--- src/main/java/com/lms/bytecoders/Controllers/Lecturer/LectureCoursesController.java
+++ src/main/java/com/lms/bytecoders/Controllers/Lecturer/LectureCoursesController.java
@@ -1,4 +1,73 @@
 package com.lms.bytecoders.Controllers.Lecturer;
 
-public class LectureCoursesController {
+import com.lms.bytecoders.Controllers.Base.BaseController;
+import com.lms.bytecoders.Services.Database;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class LectureCoursesController extends BaseController implements Initializable {
+
+    @FXML
+    private FlowPane coursesPane;
+
+    @FXML
+    private AnchorPane MainPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loadCourses();
+    }
+
+    private void loadCourses() {
+        coursesPane.setOrientation(Orientation.HORIZONTAL);
+        coursesPane.setHgap(20);
+        coursesPane.setVgap(20);
+        coursesPane.setAlignment(Pos.CENTER);
+        coursesPane.getChildren().clear();
+
+        try {
+            conn = Database.Conn();
+            String sql = """
+                            SELECT c.Course_Id, c.Course_Name
+                            FROM course c
+                            JOIN lecture_course lc ON c.Course_Id = lc.Course_Id
+                            WHERE lc.Lecturer_Id = ?;
+                    """;
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, BaseController.getUserId());
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                Label courseLabel = new Label(rs.getString("Course_Name") + "\n" + rs.getString("Course_Id"));
+                courseLabel.getStyleClass().add("course-tab");
+                courseLabel.setWrapText(true);
+
+                courseLabel.setOnMouseClicked(event -> {
+                    navigate(MainPane, "/Fxml/Lecturer/LecAddMaterial.fxml");
+                });
+
+                coursesPane.getChildren().add(courseLabel);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                System.out.println("Error in closing the Connection..." + e.getMessage());
+            }
+        }
+
+    }
 }
--- src/main/java/com/lms/bytecoders/Controllers/Student/StudentCourseController.java
+++ src/main/java/com/lms/bytecoders/Controllers/Student/StudentCourseController.java
@@ -1,20 +1,15 @@
 package com.lms.bytecoders.Controllers.Student;
 
 import com.lms.bytecoders.Controllers.Base.BaseController;
-import com.lms.bytecoders.Models.Department;
-import com.lms.bytecoders.Models.TimeTable;
 import com.lms.bytecoders.Services.Database;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 
-import java.awt.*;
-import java.net.URI;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -25,7 +20,7 @@ public class StudentCourseController extends BaseController implements Initializ
     private FlowPane coursesPane;
 
     @FXML
-    private AnchorPane stuCourseMainPane;
+    private AnchorPane MainPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -33,43 +28,35 @@ public class StudentCourseController extends BaseController implements Initializ
     }
 
     private void loadCourses() {
-        coursesPane.setOrientation(Orientation.HORIZONTAL); // Horizontal flow
-        coursesPane.setHgap(10); // Horizontal gap between nodes
-        coursesPane.setVgap(10); // Vertical gap between nodes
-        coursesPane.setAlignment(Pos.CENTER); // Center alignment
+        coursesPane.setOrientation(Orientation.HORIZONTAL);
+        coursesPane.setHgap(20);
+        coursesPane.setVgap(20);
+        coursesPane.setAlignment(Pos.CENTER);
         coursesPane.getChildren().clear();
-        sql = "SELECT * FROM time_table";
 
         try {
             conn = Database.Conn();
-            String sql = "SELECT Course_Id FROM stu_course WHERE Student_Id = ?";
+            String sql = """
+                            SELECT c.Course_Id, c.Course_Name
+                            FROM course c
+                            JOIN stu_course sc ON c.Course_Id = sc.Course_Id
+                            WHERE sc.Student_Id = ?;
+                    """;
             ps = conn.prepareStatement(sql);
             ps.setString(1, BaseController.getUserId());
             rs = ps.executeQuery();
 
             while (rs.next()) {
-                Label label = new Label("Course_Id: " + rs.getString("Course_Id"));
-                label.setStyle(
-                        "-fx-font-family: 'Segoe UI';" +
-                                "-fx-font-size: 14px;" +
-                                "-fx-font-weight: bold;" +
-                                "-fx-text-fill: linear-gradient(to right, #3498db, #2c3e50);" +
-                                "-fx-background-color: rgba(236, 240, 241, 0.8);" +
-                                "-fx-background-radius: 8px;" +
-                                "-fx-border-color: #bdc3c7;" +
-                                "-fx-border-radius: 8px;" +
-                                "-fx-border-width: 1px;" +
-                                "-fx-padding: 6px 12px 6px 12px;" +
-                                "-fx-effect: dropshadow(gaussian, rgba(0,0,0,0.3), 3, 0.5, 2, 2);"
-                );
 
-// Add hover effects
-                label.setOnMouseClicked(event -> {
-                    navigate(stuCourseMainPane, "/Fxml/Student/StudentSingleCourseView.fxml");
+                Label courseLabel = new Label(rs.getString("Course_Name") + "\n" + rs.getString("Course_Id"));
+                courseLabel.getStyleClass().add("course-tab");
+                courseLabel.setWrapText(true);
+
+                courseLabel.setOnMouseClicked(event -> {
+                    navigate(MainPane, "/Fxml/Student/StudentSingleCourseView.fxml");
                 });
-                label.setOnMouseEntered(e -> label.setStyle(label.getStyle() + "-fx-background-color: rgba(214, 234, 248, 0.9);"));
-                label.setOnMouseExited(e -> label.setStyle(label.getStyle() + "-fx-background-color: rgba(236, 240, 241, 0.8);"));
-                coursesPane.getChildren().add(label);
+
+                coursesPane.getChildren().add(courseLabel);
             }
 
         } catch (SQLException e) {
